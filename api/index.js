9acb32aae15cbc3277d585bb4a0b64a9a0f3f9d8
Joy Brar <joybrar2001@gmail.com>
2024-05-06 05:58:50 +0530

    Finishing

--- api/index.js
+++ api/index.js
@@ -6,14 +6,12 @@ const { NodeSSH } = require('node-ssh');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// CORS middleware
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
-// Replace with your GCP instance details
 const hostname = '34.170.89.197';
 const username = 'dell';
 const keyPath = path.join(__dirname, 'smollangcp');
@@ -24,46 +22,32 @@ ssh.connect({
   host: hostname,
   username: username,
   privateKeyPath: keyPath,
-})
-// .then(() => {
-//   // Install Python dependencies on GCP instance
-//   return ssh.execCommand('pip3 install -r /home/vanshaj_arora_sg/requirements.txt');
-// })
-// .then((result) => {
-  //   // Handle command execution result
-  //   console.log('STDOUT: ' + result.stdout);
-  //   console.log('STDERR: ' + result.stderr);
-  // })
-  // .catch((err) => {
-    //   // Handle connection or command execution errors
-    //   console.error('Error:', err);
-    // });
-    
-    // Function to run Python script on GCP instance
-    function runScript(filename, callback) {
-      let scriptPath;
-      if (filename === 'UnlockScript') {
-        scriptPath = '/home/vanshaj_arora_sg/UnlockScript.py';
-      } else if (filename === 'morningSetup') {
-        scriptPath = '/home/vanshaj_arora_sg/morningSetup.py';
-      } else {
-        scriptPath = '/home/vanshaj_arora_sg/testlit.py';
-      }
-      
-      ssh.execCommand(`python3 ${scriptPath}`)
-      .then((result) => {
-        callback(null, result.stdout);
-      })
-      .catch((err) => {
-        console.error('Error:', err);
-        callback(err, null);
-      });
-    }
-    
-    app.get('/api', (req, res) => {
-      res.send(`Enter script name after /script/`);
+});
+
+function runScript(filename, callback) {
+  let scriptPath;
+  if (filename === 'UnlockScript') {
+    scriptPath = '/home/vanshaj_arora_sg/UnlockScript.py';
+  } else if (filename === 'morningSetup') {
+    scriptPath = '/home/vanshaj_arora_sg/morningSetup.py';
+  } else {
+    scriptPath = '/home/vanshaj_arora_sg/testlit.py';
+  }
+
+  ssh.execCommand(`python3 ${scriptPath}`)
+    .then((result) => {
+      callback(null, result.stdout);
+    })
+    .catch((err) => {
+      console.error('Error:', err);
+      callback(err, null);
     });
-    
+}
+
+app.get('/api', (req, res) => {
+  res.send(`Enter script name after /script/`);
+});
+
 app.get('/api/script/:scriptname', (req, res) => {
   const { scriptname } = req.params;
   runScript(scriptname, (err, output) => {
@@ -71,7 +55,6 @@ app.get('/api/script/:scriptname', (req, res) => {
       res.send('Error occurred while executing script.');
       return;
     }
-    console.log(output.split('\n')[3]);
     res.send(output.split('\n')[3]);
   });
 });
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Timeline from './components/Timeline';
 import Navbar from './components/Navbar';
 
@@ -8,10 +8,10 @@ const App = () => {
     <div className="bg-zinc-50 relative">
       <Navbar />
       <div className="max-w-lg min-h-screen mx-auto pt-28 flex flex-col gap-8 justify-start bg-zinc-50 z-50">
-      <h1 className="font-semibold text-3xl text-left px-8 md:px-6 lg:px-4 xl:px-2">Steps for your Consumer App</h1>
-        <Timeline/>
+        <h1 className="font-semibold text-3xl text-left px-8 md:px-6 lg:px-4 xl:px-2">Steps for your Consumer App</h1>
+        <Timeline />
       </div>
-      <img 
+      <img
         src="/newsletter-homepage-woman-couch 1.png"
         className="absolute right-0 bottom-0 max-xl:hidden z-0"
         alt="graphic"
